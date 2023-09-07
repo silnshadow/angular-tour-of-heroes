9a67bf3084c7df96808332a1f235b2854b3414c4
Prashant Singh Vinay Kumar Singh <prashantsingh.vinaykumarsingh@msc.com>
2023-09-07 16:32:07 +0530

    Delete and Add functionality added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,32 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero';
+
+import { Hero } from '../Hero'; 
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService){}
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getHeroes();
   }
 
-  /*
-  Call it in ngOnInit()
-  While you could call getHeroes() in the constructor, 
-  that's not the best practice.
-  */
-
   getHeroes(): void {
-    this.heroService.getHeroes().
-    subscribe(heroes => this.heroes = heroes);
-  
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 
   add(name: string): void {
@@ -37,4 +30,10 @@ export class HeroesComponent implements OnInit{
         this.heroes.push(hero);
       });
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 }
