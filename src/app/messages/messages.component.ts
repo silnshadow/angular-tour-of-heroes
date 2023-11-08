d59c4a845197059743bdb4ddd6b7b6cb0bb76e4e
prashant singh <prashantsingh.vinaykumarsingh@msc.com>
2023-11-08 13:17:31 +0530

    Test meesgae created

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,6 +7,7 @@ import { MessageService } from '../message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
+  test: string = '';
   constructor(public messageService: MessageService){
 
   }
